ed617f99526274c4dc29011cddb31779c312e1ec
EmilyRead88 <read.emily88@gmail.com>
2022-04-01 11:14:04 -0400

    form is now connected to email

--- scripts.js
+++ scripts.js
@@ -3,25 +3,22 @@ const myPortfolio={};
 //create initial method to kick off the app
 myPortfolio.init = function(){
      // myPortfolio.arrowEventListener();
-     myPortfolio.formEventListener();
+     // myPortfolio.formEventListener();
      AOS.init();
 }
 
-// //create event listener to listen for click of down arrow icon
-// const downArrow = document.getElementById('scrollDown');
-// myPortfolio.arrowEventListener = function(){
-//      downArrow.addEventListener('click', function(e){
-//           e.preventDefault();
-//           const aboutSection = document.getElementById('about');
-//           aboutSection.scrollIntoView({behavior:"smooth"});
-//      })
-// }
+//for portfolio section- when overlay on event listener 'focus' - then have the div with the information appear - when not in focus, it disappears again
+     //change width of that app to 100% (and shrink the one beside it?)
+     //add display to the information attributed to that section
+          //inside of the pop up, we can now get rid of the image, and just keep title, languages, description and links 
+
+//also need to connect form to the site ppl used for forms.... 
 
 //event listeners for each of the project images (to pull up more information)
 myPortfolio.projectDivs = document.querySelectorAll('.app');
 myPortfolio.projectDivs.forEach(function(project){
      project.addEventListener('click', function(e){
-
+          //textContainer here is a display:none property
           if(e.target.className==="overlay1") {
                const runningText = document.querySelector('.runningText');
                runningText.classList.remove('textContainer');
@@ -108,33 +105,65 @@ myPortfolio.closeProjectEvent = function (clickedElement) {
 
 
 //create event listener to listen for form submit
-myPortfolio.form = document.querySelector('form');
-myPortfolio.formEventListener = function(){
-     myPortfolio.form.addEventListener('submit', function(e){
-          e.preventDefault();
-          //assign variable to in puts that user interacts with
-          const nameInput = document.getElementById('name');
-          const emailInput = document.getElementById('email');
-          const messageInput = document.getElementById('message');
-          //assign variables to set what is contained inside the inputs
-          const userName = nameInput.value;
-          const userEmail = emailInput.value;
-          const userMessage = messageInput.value;
-          //clear inputs on click
-          nameInput.value = '';
-          emailInput.value = '';
-          messageInput.value = '';
+// myPortfolio.form = document.querySelector('form');
+// myPortfolio.formEventListener = function(){
+//      myPortfolio.form.addEventListener('submit', function(e){
+//           e.preventDefault();
+//           //assign variable to in puts that user interacts with
+//           const nameInput = document.getElementById('name');
+//           const emailInput = document.getElementById('email');
+//           const messageInput = document.getElementById('message');
+//           //assign variables to set what is contained inside the inputs
+//           const userName = nameInput.value;
+//           const userEmail = emailInput.value;
+//           const userMessage = messageInput.value;
+//           //clear inputs on click
+//           nameInput.value = '';
+//           emailInput.value = '';
+//           messageInput.value = '';
 
-          const thankYouMessage = document.querySelector('.contactForm p');
-          thankYouMessage.classList.add("thankYouDisplay")
-          setTimeout(()=> {
-               thankYouMessage.classList.remove("thankYouDisplay")
-          }, 2000);
+//           const thankYouMessage = document.querySelector('.contactForm p');
+//           thankYouMessage.classList.add("thankYouDisplay")
+//           setTimeout(()=> {
+//                thankYouMessage.classList.remove("thankYouDisplay")
+//           }, 2000);
 
           
 
+//      })
+// }
+
+myPortfolio.form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+     event.preventDefault();
+     const status = document.getElementById("status");
+     const data = new FormData(event.target);
+     // console.log(data);
+     fetch(event.target.action, {
+          method: myPortfolio.form.method,
+          body: data,
+          headers: {
+               'Accept': 'application/json'
+          }
+     }).then(response => {
+          // console.log(response);
+          if (response.ok) {
+               status.innerHTML = "Thanks for reaching out!";
+               myPortfolio.form.reset();
+          } else {
+               response.json().then(data => {
+                    if(Object.hasOwn(data, 'errors')){
+                         status.innerHTML = data['errors'].map(error =>error["message"]).join(", ");
+                    } else {
+                         status.innerHTML = "Oops! There was a problem submitting your inquiry. Please try again."
+                    }
+               })
+          }
      })
 }
+myPortfolio.form.addEventListener("submit", handleSubmit)
+
 
 //call the init method
 myPortfolio.init();
