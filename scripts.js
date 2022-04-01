bbc207c2080077cee781b70722e05989a64a94fd
EmilyRead88 <read.emily88@gmail.com>
2022-04-01 15:25:26 -0400

    layout for click effect working on runningApp- need to apply to rest

--- scripts.js
+++ scripts.js
@@ -2,8 +2,6 @@ const myPortfolio={};
 
 //create initial method to kick off the app
 myPortfolio.init = function(){
-     // myPortfolio.arrowEventListener();
-     // myPortfolio.formEventListener();
      AOS.init();
 }
 
@@ -12,126 +10,65 @@ myPortfolio.init = function(){
      //add display to the information attributed to that section
           //inside of the pop up, we can now get rid of the image, and just keep title, languages, description and links 
 
-//also need to connect form to the site ppl used for forms.... 
 
 //event listeners for each of the project images (to pull up more information)
 myPortfolio.projectDivs = document.querySelectorAll('.app');
 myPortfolio.projectDivs.forEach(function(project){
      project.addEventListener('click', function(e){
           //textContainer here is a display:none property
+          //inside each IF, can we call a function that deals with all of the stuff below- can we pass it 2 arguments: runningApp and runningText? 
           if(e.target.className==="overlay1") {
                const runningText = document.querySelector('.runningText');
+               const runningApp = document.querySelector('.runningApp');
+               //below will also need to be added in to the remove event as well!
+               runningApp.style.width="100%";
+               runningApp.style.transition="1s";
                runningText.classList.remove('textContainer');
-               runningText.classList.add('appPopUp');
                myPortfolio.closeProjectEvent(runningText);
-          } else if (e.target.className==="overlay2") {
-               const restaurantText = document.querySelector('.restaurantText');
-               restaurantText.classList.remove('textContainer');
-               restaurantText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(restaurantText);
-          } else if(e.target.className==="overlay4") {
-               const colorText = document.querySelector('.colorText');
-               colorText.classList.remove('textContainer');
-               colorText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(colorText);
-          } else {
-               const blankText = document.querySelector('.blankText');
-               blankText.classList.remove('textContainer');
-               blankText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(blankText);
-          };
-          const projectOverlay = document.querySelectorAll('.app div');
-          projectOverlay.forEach(function (div) {
-               div.style.backgroundColor = "rgba(50,50,50,0.8)"
-          })
-     })
-
-     //event listener for tabbing:
-     project.addEventListener('keypress', function (e) {
-          console.log(e.key);
-          if (e.target.className === "overlay1") {
-               const runningText = document.querySelector('.runningText');
-               runningText.classList.remove('textContainer');
-               runningText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(runningText);
-          } else if (e.target.className === "overlay2") {
-               const restaurantText = document.querySelector('.restaurantText');
-               restaurantText.classList.remove('textContainer');
-               restaurantText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(restaurantText);
-          } else if (e.target.className === "overlay4") {
-               const colorText = document.querySelector('.colorText');
-               colorText.classList.remove('textContainer');
-               colorText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(colorText);
-          } else {
-               const blankText = document.querySelector('.blankText');
-               blankText.classList.remove('textContainer');
-               blankText.classList.add('appPopUp');
-               myPortfolio.closeProjectEvent(blankText);
-          };
-          const projectOverlay = document.querySelectorAll('.app div');
-          projectOverlay.forEach(function (div) {
-               div.style.backgroundColor = "rgba(50,50,50,0.8)"
-          })
+          }
+          // } else if (e.target.className==="overlay2") {
+          //      const restaurantText = document.querySelector('.restaurantText');
+          //      restaurantText.classList.remove('textContainer');
+          //      restaurantText.classList.add('appPopUp');
+          //      myPortfolio.closeProjectEvent(restaurantText);
+          // } else if(e.target.className==="overlay4") {
+          //      const colorText = document.querySelector('.colorText');
+          //      colorText.classList.remove('textContainer');
+          //      colorText.classList.add('appPopUp');
+          //      myPortfolio.closeProjectEvent(colorText);
+          // } else {
+          //      const blankText = document.querySelector('.blankText');
+          //      blankText.classList.remove('textContainer');
+          //      blankText.classList.add('appPopUp');
+          //      myPortfolio.closeProjectEvent(blankText);
+          // };
+          // const projectOverlay = document.querySelectorAll('.app div');
+          // projectOverlay.forEach(function (div) {
+          //      div.style.backgroundColor = "rgba(50,50,50,0.8)"
+          // })
      })
 })
 
-
+//close function doesn't work now because of changing layout- so needed to change popUpContainer i to textContainer i
 myPortfolio.closeProjectEvent = function (clickedElement) {
-     const closeProject = document.querySelectorAll('.popUpContainer i')
-     closeProject.forEach(function(icon){
-          icon.addEventListener('click', function () {
-               clickedElement.classList.remove('addPopUp');
-               clickedElement.classList.add('textContainer');
-               const projectOverlay = document.querySelectorAll('.app div');
-               projectOverlay.forEach(function (div) {
-                    div.style.backgroundColor = "transparent"
-               })
-          })
-          icon.addEventListener('keypress', function (e) {
-               console.log(e.key);
-               if(e.key === 'Enter'){
-               clickedElement.classList.remove('addPopUp');
-               clickedElement.classList.add('textContainer');
-                    const projectOverlay = document.querySelectorAll('.app div');
-                    projectOverlay.forEach(function (div) {
-                         div.style.backgroundColor = "transparent"
-                    })
-               }
-          })
-     })     
-}
+     console.log(clickedElement);
+     const closeProject = document.querySelector(`.fa-xmark`)
+     console.log(closeProject);
+     closeProject.addEventListener('click', function(){
+          console.log("goodbye");
+          clickedElement.classList.remove('addPopUp');
+          clickedElement.classList.add('textContainer');
+     })
+     closeProject.addEventListener('keypress', function(e){
+          console.log(e.key);
+          if(e.key === 'Enter'){
+          clickedElement.classList.remove('addPopUp');
+          clickedElement.classList.add('textContainer');
+          }
+     })
+}    
 
 
-//create event listener to listen for form submit
-// myPortfolio.form = document.querySelector('form');
-// myPortfolio.formEventListener = function(){
-//      myPortfolio.form.addEventListener('submit', function(e){
-//           e.preventDefault();
-//           //assign variable to in puts that user interacts with
-//           const nameInput = document.getElementById('name');
-//           const emailInput = document.getElementById('email');
-//           const messageInput = document.getElementById('message');
-//           //assign variables to set what is contained inside the inputs
-//           const userName = nameInput.value;
-//           const userEmail = emailInput.value;
-//           const userMessage = messageInput.value;
-//           //clear inputs on click
-//           nameInput.value = '';
-//           emailInput.value = '';
-//           messageInput.value = '';
-
-//           const thankYouMessage = document.querySelector('.contactForm p');
-//           thankYouMessage.classList.add("thankYouDisplay")
-//           setTimeout(()=> {
-//                thankYouMessage.classList.remove("thankYouDisplay")
-//           }, 2000);
-
-          
-
-//      })
-// }
 
 myPortfolio.form = document.getElementById("my-form");
 
@@ -167,3 +104,35 @@ myPortfolio.form.addEventListener("submit", handleSubmit)
 
 //call the init method
 myPortfolio.init();
+
+
+
+  // //event listener for tabbing:
+     // project.addEventListener('keypress', function (e) {
+     //      console.log(e.key);
+     //      if (e.target.className === "overlay1") {
+     //           const runningText = document.querySelector('.runningText');
+     //           runningText.classList.remove('textContainer');
+     //           runningText.classList.add('appPopUp');
+     //           myPortfolio.closeProjectEvent(runningText);
+     //      } else if (e.target.className === "overlay2") {
+     //           const restaurantText = document.querySelector('.restaurantText');
+     //           restaurantText.classList.remove('textContainer');
+     //           restaurantText.classList.add('appPopUp');
+     //           myPortfolio.closeProjectEvent(restaurantText);
+     //      } else if (e.target.className === "overlay4") {
+     //           const colorText = document.querySelector('.colorText');
+     //           colorText.classList.remove('textContainer');
+     //           colorText.classList.add('appPopUp');
+     //           myPortfolio.closeProjectEvent(colorText);
+     //      } else {
+     //           const blankText = document.querySelector('.blankText');
+     //           blankText.classList.remove('textContainer');
+     //           blankText.classList.add('appPopUp');
+     //           myPortfolio.closeProjectEvent(blankText);
+     //      };
+     //      const projectOverlay = document.querySelectorAll('.app div');
+     //      projectOverlay.forEach(function (div) {
+     //           div.style.backgroundColor = "rgba(50,50,50,0.8)"
+     //      })
+     // })
