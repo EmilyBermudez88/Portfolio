def14f2b851abe7f236f251616a11890e201ba40
EmilyRead88 <read.emily88@gmail.com>
2022-04-12 14:42:22 -0400

    fix closing issue and added in username

--- scripts.js
+++ scripts.js
@@ -5,12 +5,6 @@ myPortfolio.init = function(){
      AOS.init();
 }
 
-//for portfolio section- when overlay on event listener 'focus' - then have the div with the information appear - when not in focus, it disappears again
-     //change width of that app to 100% (and shrink the one beside it?)
-     //add display to the information attributed to that section
-          //inside of the pop up, we can now get rid of the image, and just keep title, languages, description and links 
-
-
 //event listeners for each of the project images (to pull up more information)
 myPortfolio.projectDivs = document.querySelectorAll('.app');
 myPortfolio.projectDivs.forEach((project)=>{
@@ -58,21 +52,21 @@ myPortfolio.projectDivs.forEach((project)=>{
 
 })
 
-//Event Listener to close text on click
-myPortfolio.closeProjectEvent = function (clickedElement) {
-     const closeProject = document.querySelectorAll(`.fa-xmark`);
-     const parentDiv = clickedElement.parentElement
-     closeProject.forEach((icon)=> {
-          icon.addEventListener('click', ()=>{
-               clickedElement.classList.add('hideText');
-          })
-          icon.addEventListener('keypress', (e)=> {
-               if (e.key === 'Enter'){
-               clickedElement.classList.add('hideText');
-               }
-          })
+
+// Event Listeners to close text on click 
+myPortfolio.closeProjectEvent= function(clickedElement){
+     const icon = clickedElement.childNodes[11];
+     icon.addEventListener('click', (e)=> {
+          console.log(e);
+          clickedElement.classList.add('hideText');
      })
-}    
+     icon.addEventListener('keypress', (e)=> {
+          if (e.key === 'Enter'){
+               clickedElement.classList.add('hideText');
+          }
+     })
+}
+
 
 
 myPortfolio.form = document.getElementById("my-form");
@@ -80,7 +74,8 @@ async function handleSubmit(event) {
      event.preventDefault();
      const status = document.getElementById("status");
      const data = new FormData(event.target);
-     // console.log(data);
+     const user = document.getElementById("name").value;
+     console.log(user);
      fetch(event.target.action, {
           method: myPortfolio.form.method,
           body: data,
@@ -89,14 +84,14 @@ async function handleSubmit(event) {
           }
      }).then(response => {
           if (response.ok) {
-               status.innerHTML = "Thanks for reaching out!";
+               status.innerHTML = `Thanks ${user} for reaching out!`;
                myPortfolio.form.reset();
           } else {
                response.json().then(data => {
                     if(Object.hasOwn(data, 'errors')){
                          status.innerHTML = data['errors'].map(error =>error["message"]).join(", ");
                     } else {
-                         status.innerHTML = "Oops! There was a problem submitting your inquiry. Please try again."
+                         status.innerHTML = `Sorry ${user}! There was a problem submitting your inquiry. Please try again.`
                     }
                })
           }
@@ -107,35 +102,3 @@ myPortfolio.form.addEventListener("submit", handleSubmit)
 
 //call the init method
 myPortfolio.init();
-
-
-
-  // //event listener for tabbing:
-     // project.addEventListener('keypress', function (e) {
-     //      console.log(e.key);
-     //      if (e.target.className === "overlay1") {
-     //           const runningText = document.querySelector('.runningText');
-     //           runningText.classList.remove('textContainer');
-     //           runningText.classList.add('appPopUp');
-     //           myPortfolio.closeProjectEvent(runningText);
-     //      } else if (e.target.className === "overlay2") {
-     //           const restaurantText = document.querySelector('.restaurantText');
-     //           restaurantText.classList.remove('textContainer');
-     //           restaurantText.classList.add('appPopUp');
-     //           myPortfolio.closeProjectEvent(restaurantText);
-     //      } else if (e.target.className === "overlay4") {
-     //           const colorText = document.querySelector('.colorText');
-     //           colorText.classList.remove('textContainer');
-     //           colorText.classList.add('appPopUp');
-     //           myPortfolio.closeProjectEvent(colorText);
-     //      } else {
-     //           const blankText = document.querySelector('.blankText');
-     //           blankText.classList.remove('textContainer');
-     //           blankText.classList.add('appPopUp');
-     //           myPortfolio.closeProjectEvent(blankText);
-     //      };
-     //      const projectOverlay = document.querySelectorAll('.app div');
-     //      projectOverlay.forEach(function (div) {
-     //           div.style.backgroundColor = "rgba(50,50,50,0.8)"
-     //      })
-     // })
